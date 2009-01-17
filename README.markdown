44b9a355591401ac28a8b6d63a2f1676f5fdbec5
luisbebop <luisbebop@gmail.com>
2009-01-17 16:08:33 -0200

    Readme modified. Lib changed to use SSH instead telnet and support to speed monitor added.

---
+++ README.markdown
@@ -0,0 +1,40 @@
+Blamewidth
+===
+
+I use this script with a rails application to monitor bandwidth of my office network. I can connect to my linux server with iptables via ssh and get a snapshot of my network usage.
+This a modified script written first by stevie@slowbicycle.com.
+I used SSH instead Telnet and added support to monitor bandwidth speed.
+
+Todo list:
+===
+
+* We need get the original code, add support to ssh on top of it instead rewrite the module
+* Add rspec tests
+* Package in a gem or plugin
+* Find a way to delete tables traffic_in and traffic_out from iptables. If you connect to a server with a previously traffic_in and traffic_out tables, you'll get wrong results on speed monitor.
+* Add connection exception treatment
+* Remove singleton pattern. I only use this for my rails application. 
+* Refactory the code ;)
+
+Basic usage:
+===
+
+require 'blamewidth'
+
+# initialize blamewidth
+b = Blamewidth.instance
+
+# setup monitoring for ip range 192.168.0.100 - 150
+array_of_ips = (0..50).to_a.map{|i| "192.168.0.#{100 + i}"}
+or
+array_of_ips = ["192.168.0.5", "192.168.0.20", "192.168.0.230", "192.168.0.100", "192.168.0.101", "192.168.0.103", "192.168.0.130", "192.168.0.140", "192.168.0.150", "192.168.0.160", "192.168.0.170", "192.168.0.180"]
+b.setup('192.168.0.1', 'root', array_of_ips)
+
+# print list of hogs, sorted by biggest consumer first
+b.blame
+
+# reset bandwidth stats
+b.reset
+
+# disconnect
+b.disconnect
--- blamewidth.rb
+++ blamewidth.rb
@@ -1,77 +1,95 @@
-require 'net/telnet'
+require 'net/ssh'
+require 'rubygems'
+require 'time'
+require 'singleton'
 include Net
 
 class Blamewidth
-  attr_reader :traffic
-  
-  
-  def initialize(router_ip, username, password)
-    @router_ip = router_ip
-    @username = username
-    @password = password
+  include Singleton
+  attr_reader :traffic_and_speed
+      
+  def traffic_and_speed
+    traffic_in = get_speed(:download)
+    traffic_out = get_speed(:upload)
+    @traffic_and_speed = traffic_in.merge(traffic_out) { |k,x,y| x+y}
   end
   
-  def traffic
-    return @traffic if @traffic
-    # grab all traffic statistics and create an IP => bytes hash
-    traffic_in = retrieve_iptables_stats(true)
-    traffic_out = retrieve_iptables_stats(false)
-  
-    # merge into single hash of IP => [in_traffic, out_traffic] mappings
-    # and remove empty entries
-    @traffic = traffic_in.merge(traffic_out) { |k, v1, v2| v2 = [v1, v2] }
-    @traffic.reject!{ |k,v| v == [0,0] }
+  def get_speed(type= :download)
+    enum_type = {:download => true, :upload => false}
+    
+    traffic_first = retrieve_iptables_stats(enum_type[type])
+    sleep(1)
+    traffic_last = retrieve_iptables_stats(enum_type[type])
+    
+    seconds_elapsed = traffic_last[0] - traffic_first[0]
+    traffic_first[1].merge(traffic_last[1]) {|k,x,y| [y, ((y-x)/1024)/seconds_elapsed]}
   end
   
-  def blame
-    puts "IP ADDRESS\tIN(MB)\tOUT(MB)"
-    sort_traffic(true).reverse.each do |ip, in_and_out|
-      traffic_in, traffic_out = in_and_out
-      puts "#{ip}\t#{traffic_in / 1024 / 1024}\t#{traffic_out / 1024 / 1024}"
+  def sort_traffic_and_speed(column)
+    index = {:download => 0, :downloadspeed =>1, :upload =>2, :uploadspeed =>3}
+    traffic_and_speed.sort { |a, b| a[1][ index[column] ] <=> b[1][ index[column] ] }
+  end
+  
+  def blame(order_by=:download)
+    puts "IP ADDRESS\tIN(KB)\tDL(KB/s)\tOUT(KB)\tUL(KB/s)"
+    sort_traffic_and_speed(order_by).reverse.each do |ip, in_and_out|
+      traffic_in, download, traffic_out, upload = in_and_out
+      puts "#{ip}\t#{traffic_in/1024}\t#{download}\t#{traffic_out/1024}\t#{upload}"
     end
-  end
-  
-  def sort_traffic(ingress=true)
-    index = ingress ? 0 : 1
-    traffic.sort { |a, b| a[1][ index ] <=> b[1][ index ] }
   end
   
   def session
     return @session if @session
-    s = Net::Telnet::new( "Host" => @router_ip, "Timeout" => 5, "Prompt" => /#/ )
-    s.login(@username, @password)
-    @session = s
+    @session = Net::SSH.start(@router_ip, @username, :password => @password)
   end
 
   def reset
     # zero-out the bandwidth count for all ips
-    session.cmd('iptables -Z traffic_out')
-    session.cmd('iptables -Z traffic_in')
+    session.exec!('iptables -Z traffic_out')
+    session.exec!('iptables -Z traffic_in')
     puts "Reset traffic stats."
   end
   
-  def setup(ips)
+  def disconnect
+    @session.close if @session
+    @session = nil
+    @configured = false
+  end
+    
+  def setup(router_ip, username, password, ips)
+    return if @configured
+    @router_ip = router_ip
+    @username = username
+    @password = password
+    
     # initial setup
-    setup_commands = %w(
-      iptables -N traffic_in
-      iptables -N traffic_out
-      iptables -I FORWARD 1 -j traffic_in
-      iptables -I FORWARD 2 -j traffic_out
-    )
-    setup_commands.each { |cmd| session.cmd(cmd) }
+    setup_commands = [
+      "iptables -N traffic_in",
+      "iptables -N traffic_out",      
+      "iptables -I FORWARD 1 -j traffic_in",
+      "iptables -I FORWARD 2 -j traffic_out"
+      #"iptables -I FORWARD -i eth0 -j traffic_in",
+      #"iptables -I FORWARD -o eth0 -j traffic_out"
+    ]
+    setup_commands.each { |cmd| session.exec!(cmd) }
     
     # per-ip monitoring
     ips.each do |ip|
-      session.cmd("iptables -A traffic_in -d #{ip}")
-      session.cmd("iptables -A traffic_out -s #{ip}")
+      # delete ip before put in the chain if already exists
+      session.exec!("iptables -D traffic_in -d #{ip}")
+      session.exec!("iptables -D traffic_out -s #{ip}")
+      # put ip in the chain
+      session.exec!("iptables -A traffic_in -d #{ip}")
+      session.exec!("iptables -A traffic_out -s #{ip}")
     end
+    @configured = true
   end
 
-  private
+  #private
   
   def retrieve_iptables_stats(ingress=true)
     direction = ingress ? 'in' : 'out'
-    iptables_output = session.cmd( "iptables -L traffic_#{direction} -vnx" )
+    iptables_output = session.exec!( "iptables -L traffic_#{direction} -vnx;date -u")
     parse_iptables_stats( iptables_output, ingress)
   end
 
@@ -82,10 +100,10 @@ class Blamewidth
     lines.each do |line|
       line = line.split("\s")
   
-      # create hash of ips with their corresponding traffic7
+      # create hash of ips with their corresponding traffic
       traffic[ line[ip_column] ] = line[1].to_i if line[ip_column] =~ /\d.\d.\d.\d/
     end
-    traffic
+    [Time.parse(lines.last),traffic]
   end
 
 end
